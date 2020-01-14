aabd7212167f2ca8a3f0ba178b1f435556a90c81
joydeep1701 <joydeep1701@gmail.com>
2020-01-14 17:33:40 +0530

    Removed Lol React

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import { LolReact } from "../lib/LolReact";
-
-export const Title = LolReact.createClass({
-    render() {
-        return LolReact.createElement('h1', null, this.props.message);
-    }
-});
