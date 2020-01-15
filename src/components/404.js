7f97da5db025b94b5f78d3ba143951edfc39adb9
joydeep1701 <joydeep1701@gmail.com>
2020-01-15 19:58:02 +0530

    Updated 404

--- src/components/404.js
+++ src/components/404.js
@@ -2,6 +2,8 @@ import { createTextElement, createLink } from "../utils/createElement";
 
 export const _404Page = {
     path: '/404',
+    clickable: false,
+    title: 'Page Not Found',
     async render(params, router) {
         const rootNode = document.createElement('div');
         rootNode.appendChild(createTextElement('h2', '404: Route not found'));
