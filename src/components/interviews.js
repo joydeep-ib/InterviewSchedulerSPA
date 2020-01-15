cc19e436667b6563ea5592a741b001f03c0c26e1
joydeep1701 <joydeep1701@gmail.com>
2020-01-15 19:58:56 +0530

    Added Interview Listing

--- src/components/interviews.js
+++ src/components/interviews.js
@@ -1,14 +1,66 @@
-import { createTextElement, createLink } from "../utils/createElement";
+import { createTextElement, createLink, createNavBar, createDIV, createInterviewCard } from "../utils/createElement";
+import './interview.scss';
 
 export const InterviewsPage = {
     path: '/interviews',
+    clickable: true,
+    title: 'Interviews',
     async render(params, router) {
         const rootNode = document.createElement('div');
-        rootNode.appendChild(createTextElement('h2', 'Interviews Page'));
-        rootNode.appendChild(createLink(router, 'Go Back Home', ['']))
+        rootNode.appendChild(createNavBar(router));
+
+        const _container = createDIV('container');
+        _container.appendChild(document.createElement('br'));
+
+        _container.appendChild(createTextElement('h2', 'Interviews Page'));
+        _container.appendChild(document.createElement('hr'));
+        _container.appendChild(document.createElement('br'));
+        const _newButton = createLink(router, 'Schedule New', ['interviews', 'new'], "btn btn-primary btn-md");
+        _newButton.setAttribute('role', 'button');
+        _container.appendChild(_newButton);
+
+
+        const _content = createDIV('row');
+        _content.id = 'ib_content';
+        _content.appendChild(createTextElement('p', 'Loading', 'card-container'));
+        _container.appendChild(_content);
+        rootNode.appendChild(_container);
+
         return rootNode;
     },
-    async afterRender() {
+    async afterRender(router) {
+        const _content = document.getElementById('ib_content');
         console.log("After Render Called");
+        await (new Promise((res, rej) => setTimeout(res(), 1000)));
+        const _interviewList = await fetch('http://5e1c4ab6db8a52001414ccdd.mockapi.io/api/v1/Interviews');
+        const _interviewListJSON = await _interviewList.json();
+        _content.innerHTML = '';
+        _interviewListJSON.forEach((interview, i) => {
+            const _column = createDIV('col-sm-4 card-container');
+            _column.appendChild(createInterviewCard(router, interview, i));
+            _content.appendChild(_column);
+        });
+        console.log(_interviewListJSON);
     }
-};4
+};
+
+export const NewInterviewPage = {
+    path: '/interviews/new',
+    clickable: false,
+    title: 'New Interview',
+    async render(params, router) {
+        const _rootNode = createDIV('');
+        _rootNode.appendChild(createNavBar(router));
+
+        const _container = createDIV('container');
+        _container.appendChild(document.createElement('br'));
+
+        _container.appendChild(createTextElement('h2', 'Schedule New Interview'));
+
+        _rootNode.appendChild(_container);
+        return _rootNode;
+    },
+    async afterRender(router) {
+
+    }
+};
